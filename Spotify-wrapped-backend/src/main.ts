f1375cade0a5a78dc3552a50e3e0eb3906a636e4
Juanka <62060237+icyjkk@users.noreply.github.com>
2024-12-28 21:23:05 +0100

    Update main.ts

--- Spotify-wrapped-backend/src/main.ts
+++ Spotify-wrapped-backend/src/main.ts
@@ -2,22 +2,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'; // Import dotenv
-import * as cors from 'cors'; // Import cors
 
 dotenv.config(); // Load variables from .env file
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configuración de CORS con la librería cors
-  app.use(
-    cors({
-      origin: process.env.FRONTEND_URI || '*', // Cambiar '*' a la URL específica en producción
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
-      credentials: true, // Si necesitas enviar cookies o datos de autenticación
-    }),
-  );
+  // Configuración de CORS
+  app.enableCors({
+    origin: '*', // Aqui en un futuro se pondría la URL del frontend, esto es para que solo nuestro frontend pueda hacerle peticiones a nuestro backend.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
 
   console.log(`CORS enabled for origin: ${process.env.FRONTEND_URI}`);
 
