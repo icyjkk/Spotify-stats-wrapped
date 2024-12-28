202257b19d3efc82152cd36491c025bd530b5333
icyjkk <jcdelgado5.0006@yahoo.es>
2024-12-28 20:32:19 +0100

    refactor: update Vercel configuration and remove preflight middleware
    - Changed the source and destination paths in vercel.json from 'src/main.ts' to 'dist/main.ts' for deployment.
    - Removed the middleware for handling preflight (OPTIONS) requests in main.ts to streamline CORS configuration.

--- Spotify-wrapped-backend/src/main.ts
+++ Spotify-wrapped-backend/src/main.ts
@@ -11,24 +11,6 @@ async function bootstrap() {
   // Usar validaciones globales
   app.useGlobalPipes(new ValidationPipe());
 
-  // Middleware para manejar solicitudes preflight (OPTIONS)
-  app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URI);
-      res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET,HEAD,PUT,PATCH,POST,DELETE',
-      );
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Accept, Authorization',
-      );
-      res.status(204).send();
-    } else {
-      next();
-    }
-  });
-
   // Configuración de CORS
   app.enableCors({
     origin: '*', // Aqui en un futuro se pondría la URL del frontend, esto es para que solo nuestro frontend pueda hacerle peticiones a nuestro backend.
